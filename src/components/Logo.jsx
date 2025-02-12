d194c7fca7ddf165d73fa0ee4389e6a71d40c444
Eloísa <eloisamartins.trabalho@gmail.com>
2025-02-12 20:46:51 -0300

    Setup inicial criado e inicializando funcionalidades de login com firebase

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div>
-      <img src="src/assets/logo.png" alt="Logo" className="w-32 ml-16" />
+      <img src="src/assets/logo.png" alt="Logo" className="w-32 ml-10 md:ml-16" />
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import "../styles/login.css";
 
+//Importação de componentes reutilizáveis
 import BackgroundLogin from "../components/BackgroundLogin";
 import Logo from "../components/Logo";
 
+//Componentes de Login que exibe a tela inicial do app
 const Login = () => {
   return (
     <div>
       <BackgroundLogin>
         <div className="absolute">
+          {/* Exibe o logo do aplicativo */}  
           <Logo />
           <div className="flex">
-            <div className="p-20 mt-16">
-              <h1 className="text-6xl">Welcome to Arcadia</h1>
-              <h5 className="text-xl mt-10 mr-16">
+            <div className="p-10 md:mt-16 md:p-20">
+              {/* Título principal da tela de login */}  
+              <h1 className="text-5xl md:text-6xl">Welcome to Arcadia</h1>
+
+              {/* Descrição do aplicativo */}
+              <h5 className="text-xs mt-10 mr-56 md:mr-16 md:text-xl">
               Discover a world where gaming knows no limits. From timeless classics to the latest releases, Arcadia brings you the best in entertainment. Dive into a universe of adventures, challenges, and endless fun. Are you ready to play?
               </h5>
-              <h6 className="font-bold mt-8">Create Your Free Account & Start Playing</h6>
+              {/* Chamada para ação para criar uma conta */}
+              <h6 className="font-bold md:mt-8 text-sm md:text-base">Create Your Free Account & Start Playing</h6>
             </div>
-            <div className="container-login"></div>
+            {/* Seção para exibir o formulário de login */}
+            <div className="container-login mt-60 md:-mt-10 md:mr-12"></div>
           </div>
         </div>
       </BackgroundLogin>
