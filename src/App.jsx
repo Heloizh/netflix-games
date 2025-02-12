d4dcd0096687a2f949a488d394e2c5cc9fc4e0e0
Eloísa <eloisamartins.trabalho@gmail.com>
2025-02-12 12:50:15 -0300

    Preparando ambiente

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <>
+    <div className="">Oi</div>
+    </>
+  );
+}
+
+export default App;
