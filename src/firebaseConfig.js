870281ff81a9f612dba6cf1db60b26db2859f89c
Heloizh Martins <eloisamartins.trabalho@gmail.com>
2025-02-12 21:00:24 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqeHEe9IwPG7kWJ4JyMDkZo2rieU0vIOA",
-  authDomain: "login-arcadia-aa09b.firebaseapp.com",
-  projectId: "login-arcadia-aa09b",
-  storageBucket: "login-arcadia-aa09b.firebasestorage.app",
-  messagingSenderId: "171895796227",
-  appId: "1:171895796227:web:e94e8b80d961d38e9d1436",
-  measurementId: "G-33ZCN0HXJY"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
