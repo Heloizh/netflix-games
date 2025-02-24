ae5733d74b9f059f9d125d3e3331ea2912c434b6
Eloísa <eloisamartins.trabalho@gmail.com>
2025-02-24 15:01:54 -0300

    Refazendo arquivo firebase e cuidando de arquivos sensiveis

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,7 +1,19 @@
+<<<<<<< HEAD
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 
+=======
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+>>>>>>> 84fb12f (Refazendo arquivo firebase e cuidando de arquivos sensiveis)
 const firebaseConfig = {
   apiKey: "AIzaSyCqeHEe9IwPG7kWJ4JyMDkZo2rieU0vIOA",
   authDomain: "login-arcadia-aa09b.firebaseapp.com",
@@ -9,6 +21,7 @@ const firebaseConfig = {
   storageBucket: "login-arcadia-aa09b.firebasestorage.app",
   messagingSenderId: "171895796227",
   appId: "1:171895796227:web:e94e8b80d961d38e9d1436",
+<<<<<<< HEAD
   measurementId: "G-33ZCN0HXJY"
 };
 
@@ -17,3 +30,11 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export{ auth, app };                 
+=======
+  measurementId: "G-33ZCN0HXJY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+>>>>>>> 84fb12f (Refazendo arquivo firebase e cuidando de arquivos sensiveis)
